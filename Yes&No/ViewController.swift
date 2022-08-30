8d76a0dfbd89cd65939fe6623190fc1cfe20b59b
Pigeonva <77341488+Pigeonva@users.noreply.github.com>
2022-08-30 18:42:16 +0300

    Initial Commit

---
+++ Yes&No/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yes&No
+//
+//  Created by Артур Фомин on 30.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
