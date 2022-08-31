3f1ee9fbf8ab097b48ed487d6547280e060ebb2a
Pigeonva <77341488+Pigeonva@users.noreply.github.com>
2022-08-31 20:59:17 +0300

    feat: create models properties

--- Yes&No/Models/Category.swift
+++ Yes&No/Models/Category.swift
@@ -5,8 +5,12 @@
 //  Created by Артур Фомин on 31.08.2022.
 //
 
-import Foundation
+import UIKit
 
 struct Category {
+    
+    let image: UIImage
+    let title: String
+    let dedcription: String
     
 }
