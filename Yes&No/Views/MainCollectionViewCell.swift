698ccb6dc9d64d1afab7fce0bfd826bebf18b3b1
Pigeonva <77341488+Pigeonva@users.noreply.github.com>
2022-09-01 11:01:56 +0300

    feat: create main collection view cell UI elements

--- Yes&No/Views/MainCollectionViewCell.swift
+++ Yes&No/Views/MainCollectionViewCell.swift
@@ -10,4 +10,38 @@ import UIKit
 class MainCollectionViewCell: UICollectionViewCell {
 
     weak var viewModel: CollectionViewCellViewModelType?
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints =  false
+        imageView.backgroundColor = .clear
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .black
+        
+        return imageView
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.frame = CGRect(x: 0, y: 0, width: 100, height: 20)
+        label.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        label.numberOfLines = 0
+        label.text = "Title"
+        
+        return label
+    }()
+    
+    let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.frame = CGRect(x: 0, y: 0, width: 100, height: 20)
+        label.font = UIFont.systemFont(ofSize: 20, weight: .medium)
+        label.numberOfLines = 0
+        label.text = "This description tells about something interesting"
+        
+        return label
+    }()
+    
 }
