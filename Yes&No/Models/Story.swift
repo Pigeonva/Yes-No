7515bfdaf021029647f8be332bfea48502a90641
Pigeonva <77341488+Pigeonva@users.noreply.github.com>
2022-09-02 20:32:01 +0300

    feat: create Story model

---
+++ Yes&No/Models/Story.swift
@@ -0,0 +1,17 @@
+//
+//  Story.swift
+//  Yes&No
+//
+//  Created by Артур Фомин on 02.09.2022.
+//
+
+import UIKit
+
+struct Story {
+    
+    let image: UIImage
+    let title: String
+    let story: String
+    let answer: String
+    
+}
