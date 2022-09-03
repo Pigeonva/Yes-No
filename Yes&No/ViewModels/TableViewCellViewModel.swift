fa7a5a9ba74ce37f8893c1943a96f399bad405f3
Pigeonva <77341488+Pigeonva@users.noreply.github.com>
2022-09-03 17:19:03 +0300

    feat: create view model and protocol for cell

---
+++ Yes&No/ViewModels/TableViewCellViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  TableViewCellViewModel.swift
+//  Yes&No
+//
+//  Created by Артур Фомин on 03.09.2022.
+//
+
+import UIKit
+
+class TableViewCellViewModel: TableViewCellViewModelType {
+    
+}
